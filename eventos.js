0036e415cb40114e380a340749e04afdd5c12a0e
Buruno18 <maffeib18@gmail.com>
2023-09-01 16:48:48 -0300

    botón div

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const btnDiv = document.getElementById('btnDiv');
+
+    btnDiv.addEventListener("click", function () {
+        alert("Hola! Soy el div");
+    });
+})
